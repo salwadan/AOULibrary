1c084e0353f50407212a3839db0368e0b1ab6eca
NejoudBatawaih <n_m_b2010@hotmail.cpm>
2024-11-06 17:21:35 +0300

    njood change

---
+++ lib/Courses/courses.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:salwa_app/Courses/coursepage.dart';
+import 'package:comment_box/comment/comment.dart';
+import 'package:comment_box/comment/test.dart';
+import 'package:comment_box/main.dart';
+
+class Courses extends StatefulWidget {
+  const Courses({super.key});
+
+  @override
+  State<Courses> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<Courses> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Courses',
+            style: TextStyle(color: Colors.blue),
+          ),
+          bottom: TabBar(tabs: [
+            Tab(
+              child: Text('Material', style: TextStyle(color: Colors.blue),),
+
+            ),
+            Tab(
+              child: Text('Feedback', style: TextStyle(color: Colors.blue),
+              ),
+            )
+          ]),
+        ),
+        body: Container(
+          padding: EdgeInsets.all(10),
+          child: TabBarView(children: [
+            CoursePage(courseName: "courseName"),
+            TestMe(),
+          ]),
+        ),
+      ),
+
+    );
+  }
+}
